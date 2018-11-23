b884b55b142dd5dd540f1dca2ad5f51bc8fb2ae7
gex713 <gex713@yahoo.com>
2018-11-23 12:21:18 -0500

    Group Work
    Adding the work we did in the group session last Tuesday.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private int SPEED = 500;
+	// Use this for initialization
+	void Start () {
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (rb2d != null)
+        {
+            float xMovement = Input.GetAxis("Horizontal");
+
+            if (xMovement > .2f || xMovement < -.2f)
+            {
+                rb2d.AddForce(Vector2.right * xMovement * Time.deltaTime * SPEED);
+            }
+
+            
+        }
+            
+
+	}
+}
