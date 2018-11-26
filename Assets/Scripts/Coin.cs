4ddc0ad506742cc172248af08615a0f6abb85408
zhollis21 <zhollis21@gmail.com>
2018-11-25 19:17:10 -0500

    Fixed Tile Spacing Issue, Added Player Animations and Coins
    Tile Spacing: Edited the grassy sprites to fix spacing issues.
    
    Player: Added movement animation and jump functionality.
    
    Coin: Added a coin with a rotating animation and a collectability functionality.

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    // When the player enters the collider of the coin, we destroy it.
+    // ToDo: We will probably have this add to a score later
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
