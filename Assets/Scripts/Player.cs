6d5471b3b4229cbe9f8ed758b5b87b119489683c
zhollis21 <zhollis21@gmail.com>
2018-11-30 22:44:53 -0500

    Added camera control and a game manager instance
    GameManager.cs - Added a singleton style game manager that controls the camera movement.
    
    Player.cs - Renamed to match our current conventions.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public Text scoreText;
+
+    private Rigidbody2D rb2d;
+    private const int MOVEMENT_SPEED = 500;
+    private const int JUMP_SPEED = 500;
+    private Animator playerAnimator;
+    private SpriteRenderer playerRenderer;
+    private bool IsOnTheGround;
+    private int score;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb2d != null)
+        {
+            // Handle Horizontal Movement
+            float xMovement = Input.GetAxisRaw("Horizontal");
+
+            if (xMovement > .2f || xMovement < -.2f)
+            {
+                rb2d.AddForce(Vector2.right * xMovement * Time.deltaTime * MOVEMENT_SPEED);
+                playerAnimator.SetBool("Walking", true);
+
+                playerRenderer.flipX = xMovement < 0; // If we are walking left, flip the animation
+            }
+            else
+            {
+                playerAnimator.SetBool("Walking", false);
+                
+                if (IsOnTheGround) // Stop the player from sliding on the ground
+                    rb2d.velocity = Vector2.up * rb2d.velocity.y;
+            }
+
+
+            // Handle Vertical Movement
+            float jumpMovement = Input.GetAxis("Jump");
+
+            if (jumpMovement > .5 && IsOnTheGround)
+            {
+                rb2d.AddForce(Vector2.up * JUMP_SPEED);
+                IsOnTheGround = false;
+            }
+
+            // Save the player if they fall off
+            if (transform.position.y < -15)
+            {
+                transform.position = Vector3.zero;
+
+                // Take away a point for saving them
+                if (score > 0)
+                {
+                    score--;
+                    scoreText.text = "Score: " + score;
+                }
+            }
+
+            GameManager.instance.SetCameraPosition(transform.position);
+        }
+    }
+
+    public void AddToScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score: " + score;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            IsOnTheGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            IsOnTheGround = false;
+    }
+}
