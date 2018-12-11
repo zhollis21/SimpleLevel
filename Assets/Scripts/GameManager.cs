5f4654032c6b5ab8d9f60c4e372e2bd56e1e850c
Zach Hollis <zhollis21@gmail.com>
2018-12-10 23:17:12 -0500

    Added a pause menu
    Press escape to try it.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -62,6 +63,11 @@ public class GameManager : MonoBehaviour
             score--;
             scoreText.text = "Score: " + score;
         }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void CheckForOutOfBounds()
