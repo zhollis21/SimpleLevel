68197483b4d2006317e3044209a2c12d2c03b7aa
Zach Hollis <zhollis21@gmail.com>
2018-12-27 15:46:14 -0500

    Added hidden areas (I'll show you how to use them later)

---
+++ Assets/HiddenArea.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HiddenArea : MonoBehaviour
+{
+
+    private TilemapRenderer mapRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        mapRenderer = GetComponent<TilemapRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (mapRenderer != null)
+        {
+            mapRenderer.sortingLayerName = "Default";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (mapRenderer != null)
+        {
+            mapRenderer.sortingLayerName = "Hidden Area";
+        }
+    }
+}
