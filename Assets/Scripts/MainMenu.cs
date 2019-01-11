cd888606248cabb6e407c876d5d8e98895302d9b
Zach Hollis <zhollis21@gmail.com>
2019-01-10 21:37:19 -0500

    Added Simple Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGameClicked()
+    {
+        //ToDo: Replace with Level 1
+        SceneManager.LoadScene("Zach's Scene");
+    }
+
+    public void LevelSelectionClicked()
+    {
+        //ToDo: Create Level Selction Menu
+    }
+
+    public void OptionsClicked()
+    {
+        //ToDo: Create Options
+    }
+}
