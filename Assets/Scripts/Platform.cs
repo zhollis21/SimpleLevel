dfe023ad7430d7f39015a4bb67c20edeff75f501
Zach Hollis <zhollis21@gmail.com>
2019-01-23 21:30:07 -0500

    Fixed the player sliding and bouncing on the platform
    This is a common problem and it seems the most common solution is to make the player a child of the platform while colliding with it and then removing the player when they are no longer colliding.

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -101,4 +101,14 @@ public class Platform : MonoBehaviour
             }
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(null);
+    }
 }
