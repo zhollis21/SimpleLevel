75620fd0fee2db2c5d89e88430b4cdc75d3353cb
Zach Hollis <zhollis21@gmail.com>
2019-02-28 20:00:39 -0500

    Added ice platform and fixed some minor issues
    Added ice platform
    
    Fixed the settings on the other platform sprites
    
    GameUI is enabled by default, but not the pause menu
    
    Added my finished level as grass world lvl 4
    Fixed an issue with the template
    
    My scene has been overritten with the template version for me to start working on the next level (candy)
    
    Fixed an issue with the coin class

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,13 +6,15 @@ public class Coin : MonoBehaviour
 {
 
     public int value;
+    private bool collected = false;
 
     // When the player enters the collider of the coin, we send a score update and destroy the coin.
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (!collected && collision.tag == "Player")
         {
             GameManager.instance.AddToScore(value);
+            collected = true;
             Destroy(gameObject);
         }
     }
