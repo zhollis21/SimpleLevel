4f33f668a861dc295cf7c34db18c330b5e56e687
Zach Hollis <zhollis21@gmail.com>
2019-03-11 13:01:21 -0400

    Grass Overworld is coming along

---
+++ Assets/Scripts/LevelEntrance.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEntrance : MonoBehaviour
+{
+    public enum Direction { Forward, Backward, None };
+
+    public Direction up;
+    public Direction down;
+    public Direction right;
+    public Direction left;
+
+    public List<Vector2> forwardDirections;
+    public List<Vector2> backwardDirections;
+}
---
+++ Assets/Scripts/LevelSelectionPlayer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectionPlayer : MonoBehaviour
+{
+    public string sceneNameOfNextOverworld;
+    public string sceneNameOfPreviousOverworld;
+    public List<LevelEntrance> levels;
+
+    private const float BUTTON_FORCE = .1f;
+    private int levelIndex;
+    private bool IsMoving;
+    private List<Vector2> movingDirections;
+    private int movingIndex;
+    private const float MOVEMENT_SPEED = 40;
+    private int direction;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Assert(levels != null, "Levels on the player are null!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LevelEntrance selectedLevel = levels[levelIndex];
+
+        if (!IsMoving)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            float horizontalInput = Input.GetAxis("Horizontal");
+
+            if (verticalInput > BUTTON_FORCE && selectedLevel.up != LevelEntrance.Direction.None)
+            {
+                SetupMovement(selectedLevel, selectedLevel.up);
+            }
+            else if (verticalInput < -BUTTON_FORCE && selectedLevel.down != LevelEntrance.Direction.None)
+            {
+                SetupMovement(selectedLevel, selectedLevel.down);
+            }
+            else if (horizontalInput > BUTTON_FORCE && selectedLevel.right != LevelEntrance.Direction.None)
+            {
+                SetupMovement(selectedLevel, selectedLevel.right);
+            }
+            else if (horizontalInput < -BUTTON_FORCE && selectedLevel.left != LevelEntrance.Direction.None)
+            {
+                SetupMovement(selectedLevel, selectedLevel.left);
+            }
+        }
+        else
+        {
+            transform.localPosition = Vector2.MoveTowards(transform.localPosition, movingDirections[movingIndex], MOVEMENT_SPEED * Time.deltaTime);
+
+            if (Vector2.Distance(transform.localPosition, movingDirections[movingIndex]) < .1)
+                movingIndex += direction;
+
+            if (movingIndex == -1)
+            {
+                levelIndex--;
+                IsMoving = false;
+            }
+            else if (movingIndex == movingDirections.Count)
+            {
+                levelIndex++;
+                IsMoving = false;
+            }
+        }
+    }
+
+    private void SetupMovement(LevelEntrance selectedLevel, LevelEntrance.Direction directionOfMovement)
+    {
+        if (directionOfMovement == LevelEntrance.Direction.Forward)
+        {
+            movingDirections = selectedLevel.forwardDirections;
+            direction = 1;
+            movingIndex = 0;
+        }
+        else
+        {
+            movingDirections = selectedLevel.backwardDirections;
+            direction = -1;
+            movingIndex = movingDirections.Count - 1;
+        }
+        IsMoving = true;
+    }
+}
